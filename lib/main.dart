9860a5aa6b6b10ee07f126785e3226670860a7a6
muriloMiranda404 <murilomiranda404@gmail.com>
2025-07-17 15:36:15 -0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,29 @@
-import "dart:io";
+import "dart:io" show Platform;
 
 import "package:flutter/material.dart";
 import "package:window_manager/window_manager.dart";
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 //tipo o robotInit ou teleopInit
   void main() async{
     windowManager.ensureInitialized();//garante que tudo esteja pronto antes de iniciar
-    if(Platform.isWindows){
+    if(!kIsWeb && Platform.isWindows){
       await windowManager.ensureInitialized();
-    }
 
+      WindowOptions options = const WindowOptions(
+        size: Size(500, 450),
+        backgroundColor: Colors.green,
+        minimumSize: Size(500, 450),
+        center: true,
+      );
+
+      windowManager.waitUntilReadyToShow(options, () async{
+        windowManager.focus();
+        windowManager.maximize();
+        windowManager.show();
+      });
+    }
+      runApp(const MyApp());
     }
 
   class MyApp extends StatefulWidget{
@@ -19,14 +33,29 @@ import "package:window_manager/window_manager.dart";
     State<MyApp> createState() => MyAppState();
   }
 
-  class MyAppState extends State<MyApp>{
-    String modo = "autonomous";
-    
+  class MyAppState extends State<MyApp>{    
 
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-
+      title: 'Hyobots FRC table',
+      theme: ThemeData(
+        colorScheme: ColorScheme.dark(),
+        useMaterial3: true,
+      ),
+      home: getApp(),
     );
+  }
+
+   Widget getApp(){
+    if(Platform.isAndroid){
+      Stack(
+        alignment: Alignment.center,
+        children: [
+          Text("erro")
+        ],
+      );
+    }
+    return widget;
   }
   }
